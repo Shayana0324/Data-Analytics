2ec9f3a111daf20698b0f1039ace2ace066063a1
Shayana Shrestha <shayana0324@gmail.com>
2026-05-24 18:53:04 -0500

    Series in Pandas

--- Pandas/Creating_Reading_Writing.py
+++ Pandas/Creating_Reading_Writing.py
@@ -4,5 +4,16 @@ import pandas as pd
 df = pd.DataFrame({'Yes': [50,21], 'No': [131, 2]})
 print(df)
 
-df_name = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+df_name = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
+                        'Sue': ['Pretty good.', 'Bland.']},
+                        index=['Product A', 'Product B'])
 print(df_name)
+
+# Series
+series = pd.Series([1, 2, 3, 4, 5])
+print(series)
+
+series_sales = pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
+print(series_sales)
+
+# Reading data files
