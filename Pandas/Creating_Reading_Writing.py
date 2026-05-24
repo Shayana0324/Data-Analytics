67c46396b584844ba19737216eb4baadf5f4ff04
Shayana Shrestha <shayana0324@gmail.com>
2026-05-24 18:48:47 -0500

    Pandas and creating, reading, writing

---
+++ Pandas/Creating_Reading_Writing.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Creating data
+df = pd.DataFrame({'Yes': [50,21], 'No': [131, 2]})
+print(df)
+
+df_name = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+print(df_name)
