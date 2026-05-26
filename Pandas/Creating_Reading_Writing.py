608997f8a91257302e1a45e5b53ea83492f2e50f
Shayana Shrestha <shayana0324@gmail.com>
2026-05-26 14:07:02 -0400

    Reading a csv file

--- Pandas/Creating_Reading_Writing.py
+++ Pandas/Creating_Reading_Writing.py
@@ -17,3 +17,6 @@ series_sales = pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 
 print(series_sales)
 
 # Reading data files
+wine_reviews = pd.read_csv("./winemag-data-130k-v2.csv", index_col=0)
+print(wine_reviews.shape)
+print(wine_reviews.head())
