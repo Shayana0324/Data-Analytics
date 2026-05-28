6b75e0b183df5a566fa1b7d21e91a7a1d11170f1
Shayana Shrestha <shayana0324@gmail.com>
2026-05-27 21:45:19 -0400

    Sorting

--- Pandas/Grouping_and_Sorting.py
+++ Pandas/Grouping_and_Sorting.py
@@ -6,5 +6,26 @@ reviews = pd.read_csv("./winemag-data-130k-v2.csv", index_col=0)
 print(reviews.groupby('points').points.count())
 
 print(reviews.groupby('points').price.min())
+# Groupwise analysis - apply()
 print(reviews.groupby('winery').apply(lambda df: df.title.iloc[0]))
-print(reviews.groupby('country', 'province').apply(lambda df: df.loc[df.points.idmax()]))
+# Groupwise analysis - more than one column
+print(reviews.groupby(['country', 'province']).apply(lambda df: df.loc[df.points.idxmax()]))
+# Groupwise analysis - agg()
+print(reviews.groupby(['country']).price.agg([len, min, max]))
+
+# Multi-indexes
+countries_reviewed = reviews.groupby(['country', 'province']).description.agg([len])
+print(countries_reviewed)
+
+mi = countries_reviewed.index
+print(type(mi))
+
+print(countries_reviewed.reset_index())
+
+# Sorting
+countries_reviewed = countries_reviewed.reset_index()
+print(countries_reviewed.sort_values(by='len'))
+# sort_values()
+print(countries_reviewed.sort_values(by='len', ascending=False))
+print(countries_reviewed.sort_index())
+print(countries_reviewed.sort_values(by=['country', 'len']))
