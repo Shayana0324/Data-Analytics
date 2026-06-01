f641b2055e9e351812d98ca63fbb758a66039c4f
Shayana Shrestha <shayana0324@gmail.com>
2026-06-01 16:51:00 -0400

    Parsing Dates

--- Data Cleaning/Parsing_Dates.py
+++ Data Cleaning/Parsing_Dates.py
@@ -10,3 +10,23 @@ landslides = pd.read_csv("./input/catalog.csv")
 np.random.seed(0)
 
 print(landslides.head())
+
+# print first few rows of the date column
+print(landslides['date'].head())
+print(landslides['date'].dtype)
+
+# create a new column, date_parsed, with the parsed dates
+landslides['date_parsed'] = pd.to_datetime(landslides
+['date'], format="%m/%d/%y")
+print(landslides['date_parsed'].head())
+# landslides['date_parsed'] = pd.to_datetime(landslides['Date'], infer_datetime_format=True)
+
+# get the day of the month from the date_parsed column
+day_of_month_landslides = landslides['date_parsed'].dt.day
+print(day_of_month_landslides.head())
+
+# remove na's
+day_of_month_landslides = day_of_month_landslides.dropna()
+
+# plot the day of the month
+sns.displot(day_of_month_landslides, kde=False, bins=21)
